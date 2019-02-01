e7d8018500947abf9741706f453152910acdb7ba
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-02-01 12:10:46 -0800

    Improve event capture

--- overlay/startup.sh
+++ overlay/startup.sh
@@ -6,20 +6,25 @@
 trap "echo 'Terminating'; killall inotifywait smbd nmbd; exit" TERM
 
 CONF=/etc/samba/shareable.conf
-while true; do
-  echo > ${CONF}
-  for name in /shareable/* ; do
-    if [ -d "$name" ]; then
-      echo "[$(basename "${name}")]" >> ${CONF}
-      echo "  path=${name}" >> ${CONF}
-      echo "  public = yes" >> ${CONF}
-      echo "  writable = yes" >> ${CONF}
-      echo "  printable = no" >> ${CONF}
-      echo "  browseable = yes" >> ${CONF}
-      echo "  guest ok = yes" >> ${CONF}
-    fi
+
+mkconf() {
+  while true; do
+    echo > ${CONF}
+    for name in /shareable/* ; do
+      if [ -d "$name" ]; then
+        echo "[$(basename "${name}")]" >> ${CONF}
+        echo "  path=${name}" >> ${CONF}
+        echo "  public = yes" >> ${CONF}
+        echo "  writable = yes" >> ${CONF}
+        echo "  printable = no" >> ${CONF}
+        echo "  browseable = yes" >> ${CONF}
+        echo "  guest ok = yes" >> ${CONF}
+      fi
+    done
+    killall -HUP smbd
+    read event
   done
-  killall -HUP smbd
-  inotifywait -e modify,create,delete /shareable/ &
-  wait "$!"
-done
+}
+
+inotifywait -m -e modify,create,delete /shareable/ | mkconf &
+wait "$!"
