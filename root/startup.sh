fd7753bdc5b048b01c57c261fa3580cda374053b
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-02 22:24:35 -0800

    Remove any data from delete shared I created

--- root/startup.sh
+++ root/startup.sh
@@ -18,8 +18,13 @@ done
 
 IFS='
 '
+mkdir -p /tmp/links
+for name in $(ls /myshares) ; do
+  ln -s ${name} /tmp/links/${name}
+done
 for name in $(cat /etc/extra-shares) ; do
   mkdir -p /myshares/${name}
+  rm -rf /tmp/links/${name}
   echo "[${name}]" >> ${CONF}
   echo "  path=/myshares/${name}" >> ${CONF}
   echo "  public = yes" >> ${CONF}
@@ -27,6 +32,9 @@ for name in $(cat /etc/extra-shares) ; do
   echo "  printable = no" >> ${CONF}
   echo "  browseable = yes" >> ${CONF}
   echo "  guest ok = yes" >> ${CONF}
+done
+for name in $(ls /tmp/links) ; do
+  rm -rf /tmp/links/${name} /myshares/${name}
 done
 
 /usr/sbin/nmbd -D
