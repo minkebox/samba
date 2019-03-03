f5b519c1db5c9922018293d8deecd50147fd0137
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-03 12:35:38 -0800

    Simpler

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ RUN apk --no-cache add samba-server
 
 COPY root/ /
 
-VOLUME [ "/myshares" ]
-
 EXPOSE 137/udp 138/udp 139/tcp 445/tcp
 
 ENTRYPOINT ["/startup.sh"]
--- root/startup.sh
+++ root/startup.sh
@@ -4,7 +4,7 @@ trap "killall sleep smbd nmbd; exit" TERM INT
 
 CONF=/etc/samba/shareable.conf
 
-mkdir -p /shareable /myshares /tmp/links
+mkdir -p /shareable
 
 for name in /shareable/* ; do
   if [ -d "$name" ]; then
@@ -16,26 +16,6 @@ for name in /shareable/* ; do
     echo "  browseable = yes" >> ${CONF}
     echo "  guest ok = yes" >> ${CONF}
   fi
-done
-
-IFS='
-'
-for name in $(ls /myshares) ; do
-  ln -s ${name} /tmp/links/${name}
-done
-for name in $(cat /etc/extra-shares) ; do
-  mkdir -p /myshares/${name}
-  rm -rf /tmp/links/${name}
-  echo "[${name}]" >> ${CONF}
-  echo "  path=/myshares/${name}" >> ${CONF}
-  echo "  public = yes" >> ${CONF}
-  echo "  writable = yes" >> ${CONF}
-  echo "  printable = no" >> ${CONF}
-  echo "  browseable = yes" >> ${CONF}
-  echo "  guest ok = yes" >> ${CONF}
-done
-for name in $(ls /tmp/links) ; do
-  rm -rf /tmp/links/${name} /myshares/${name}
 done
 
 /usr/sbin/nmbd -D
